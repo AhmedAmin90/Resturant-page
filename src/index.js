081bbbc230065b87a68ca632498693179aaa6538
AhmedAmin90 <49597460+AhmedAmin90@users.noreply.github.com>
2021-03-30 00:30:25 +0300

    Add basic structure of tabs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './assets/style.css'; 
+import createTabArea from './tabs/loadPage'
 
+createTabArea()
+
+const homeBtn = document.querySelector('.home-btn')
+homeBtn.addEventListener('click' , ()=> {
+  alert('hello')
+})
+
+const menuBtn = document.querySelector('.menu-btn')
+menuBtn.addEventListener('click' , ()=> {
+  alert('hello')
+})
+
+const contactBtn = document.querySelector('.contact-btn')
+contactBtn.addEventListener('click' , ()=> {
+  alert('hello')
+})
