fd10cdae01223cad4abdb0941c3853411d52208d
AhmedAmin90 <49597460+AhmedAmin90@users.noreply.github.com>
2021-03-30 23:36:28 +0300

    Add contact page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './assets/style.css';
 import createTabArea from './tabs/loadPage';
 import  {showHomePage , hideHomePage}  from './tabs/home';
 import  {showMenuPage , hideMenuPage}  from './tabs/menu';
+import  {showcontactPage , hidecontactPage} from './tabs/contact'
 
 
 
@@ -9,17 +10,21 @@ createTabArea();
 
 const homeBtn = document.querySelector('.home-btn')
 homeBtn.addEventListener('click' , ()=> {
+hidecontactPage();
 hideMenuPage();
 showHomePage();
 })
 
 const menuBtn = document.querySelector('.menu-btn')
 menuBtn.addEventListener('click' , ()=> {
+hidecontactPage();
 hideHomePage();
 showMenuPage();
 })
 
 const contactBtn = document.querySelector('.contact-btn')
 contactBtn.addEventListener('click' , ()=> {
-  alert('hello')
+  hideHomePage();
+  hideMenuPage();
+  showcontactPage();
 })
